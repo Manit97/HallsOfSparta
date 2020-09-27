b6bfc348ae4e7999aa335be47c420c757dce0e3d
Timothy Oluwole <TOluwole@spartaglobal.com>
2020-09-27 23:26:57 +0100

    Initial commit

---
+++ src/main/java/com/sparta/halls/app/AppApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.halls.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
+
+}
