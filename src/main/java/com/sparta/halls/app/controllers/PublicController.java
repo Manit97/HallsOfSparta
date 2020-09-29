5daa3beaa0714dca0b51e4681db014522bc7af4d
Manit <manit99shah@gmail.com>
2020-09-29 15:10:13 +0100

    Frontend Basic Setup

--- src/main/java/com/sparta/halls/app/controllers/PublicController.java
+++ src/main/java/com/sparta/halls/app/controllers/PublicController.java
@@ -10,4 +10,29 @@ public class PublicController {
     public String dummyMethod() {
         return "view/publicPages/welcome";
     }
+
+    @GetMapping("welcome")
+    public String dummyMethod0() {
+        return "view/publicPages/welcome";
+    }
+
+    @GetMapping("login")
+    public String dummyMethod1() {
+        return "view/publicPages/login";
+    }
+
+    @GetMapping("enquiries")
+    public String dummyMethod2() {
+        return "view/publicPages/enquiries";
+    }
+
+    @GetMapping("halls")
+    public String dummyMethod3() {
+        return "view/publicPages/halls";
+    }
+
+    @GetMapping("selectedHall")
+    public String dummyMethod4() {
+        return "view/publicPages/selectedHall";
+    }
 }
