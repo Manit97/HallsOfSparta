4e25cf004eb91a067a3fdbf16eadd73638264e8c
Manit <manit99shah@gmail.com>
2020-09-30 09:26:45 +0100

    roomType html page added

--- src/main/java/com/sparta/halls/app/controllers/PublicController.java
+++ src/main/java/com/sparta/halls/app/controllers/PublicController.java
@@ -6,33 +6,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PublicController {
 
-    @GetMapping("/")
-    public String dummyMethod() {
-        return "view/publicPages/welcome";
-    }
-
-    @GetMapping("welcome")
-    public String dummyMethod0() {
-        return "view/publicPages/welcome";
-    }
-
-    @GetMapping("login")
-    public String dummyMethod1() {
-        return "view/publicPages/login";
-    }
-
-    @GetMapping("enquiries")
-    public String dummyMethod2() {
-        return "view/publicPages/enquiries";
-    }
-
-    @GetMapping("halls")
-    public String dummyMethod3() {
-        return "view/publicPages/halls";
-    }
-
-    @GetMapping("selectedHall")
-    public String dummyMethod4() {
-        return "view/publicPages/selectedHall";
-    }
+//    @GetMapping("/")
+//    public String dummyMethod() {
+//        return "view/publicPages/welcome";
+//    }
+//
+//    @GetMapping("welcome")
+//    public String dummyMethod0() {
+//        return "view/publicPages/welcome";
+//    }
+//
+//    @GetMapping("login")
+//    public String dummyMethod1() {
+//        return "view/publicPages/login";
+//    }
+//
+//    @GetMapping("enquiries")
+//    public String dummyMethod2() {
+//        return "view/publicPages/enquiries";
+//    }
+//
+//    @GetMapping("halls")
+//    public String dummyMethod3() {
+//        return "view/publicPages/halls";
+//    }
+//
+//    @GetMapping("selectedHall")
+//    public String dummyMethod4() {
+//        return "view/publicPages/selectedHall";
+//    }
 }
