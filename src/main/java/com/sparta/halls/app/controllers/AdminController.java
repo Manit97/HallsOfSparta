3c4fe078b8a63b1370482a08bf869d8a48a14557
ConorMalone14 <CMalone@spartaglobal.com>
2020-10-01 14:42:57 +0100

    Added student records page

--- src/main/java/com/sparta/halls/app/controllers/AdminController.java
+++ src/main/java/com/sparta/halls/app/controllers/AdminController.java
@@ -1,11 +1,16 @@
 package com.sparta.halls.app.controllers;
 
 import com.sparta.halls.app.entities.Enquiries;
+import com.sparta.halls.app.entities.Student;
 import com.sparta.halls.app.services.EnquiriesService;
+import com.sparta.halls.app.services.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -13,10 +18,12 @@ import java.util.List;
 public class AdminController {
 
     private EnquiriesService enquiriesService;
+    private StudentService studentService;
 
     @Autowired
-    public AdminController(EnquiriesService enquiriesService) {
+    public AdminController(EnquiriesService enquiriesService, StudentService studentService) {
         this.enquiriesService = enquiriesService;
+        this.studentService = studentService;
     }
 
     @GetMapping("/viewPublicEnquiries")
@@ -39,4 +46,24 @@ public class AdminController {
         model.addAttribute("enquiries", enquiries);
         return "view/adminPages/viewRoomBookings";
     }
+
+    @GetMapping("/studentRecords")
+    public String dummyMethod1(Model model) {
+        model.addAttribute("student", new Student());
+        return "view/adminPages/studentRecords";
+    }
+
+    @PostMapping("/addStudent")
+    public String addStudent(@ModelAttribute Student student, Model model){
+        studentService.addStudent(student);
+        return "view/adminPages/updateSuccess";
+    }
+
+    @PostMapping("/deleteStudent")
+    public String deleteStudent(@ModelAttribute Student student, Model model){
+        int studentId = student.getStudentId();
+        studentService.deleteStudent(studentId);
+        return "view/adminPages/updateSuccess";
+    }
+
 }
