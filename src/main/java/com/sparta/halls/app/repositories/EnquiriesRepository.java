4dcdd16a59226c9e5f8a97f71ccd4e46c7384dd4
ConorMalone14 <CMalone@spartaglobal.com>
2020-10-01 11:20:28 +0100

    added enquiries service functionality

--- src/main/java/com/sparta/halls/app/repositories/EnquiriesRepository.java
+++ src/main/java/com/sparta/halls/app/repositories/EnquiriesRepository.java
@@ -4,7 +4,9 @@ import com.sparta.halls.app.entities.Enquiries;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EnquiriesRepository extends CrudRepository<Enquiries, Integer>{
-
+    List<Enquiries> findAllByeAndEnquiryTypeId(int type);
 }
